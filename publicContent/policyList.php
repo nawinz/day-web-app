d11fd35fb9fbde79e1f6224e56c2abcd7450e276
nawinz <41543586+nawinz@users.noreply.github.com>
2021-10-24 12:32:04 +0700

    Privacy

---
+++ publicContent/policyList.php
@@ -0,0 +1,18 @@
+<?php define('usemainjs',false);define('dotdotcnt',2);define('bodyClass',''); define('titleExtend','Privacy Policy');define('footerFixed',true); require_once('./../static/Content/head.php'); ?>
+<div class='container-fluid'>
+    <div class="card bg-light privacy_policy_list_chart text-dark">
+        <div class="card-title ml-2 h3 mt-2">Privacy Policy | Days Web App</div>
+        <div class="card-body">
+        <ul class='nav flex-column'>
+            <li class='nav-item'><a class='nav-link text-primary font-weight-bold' href="../../readableContent/policy/term/read">Terms of condition</a></li>
+            <li class='nav-item'><a class='nav-link text-primary font-weight-bold' href="../../readableContent/policy/privacy/read">Data Use Policy</a></li>
+            <li class='nav-item'><a class='nav-link text-primary font-weight-bold' href='../../readableContent/policy/cookie/read'>Cookie Use</a></li>
+            <li class='nav-item'><a class='nav-link text-primary font-weight-bold' href="../../readableContent/policy/disclaimer/read">Disclaimer</a></li>
+            <li class='nav-item'><a class='nav-link text-primary font-weight-bold' href="../../readableContent/policy/returnrefund/read">Return &amp; Refunds</a></li>
+            <li class='nav-item'><a class='nav-link text-primary font-weight-bold' href="../../readableContent/policy/eula/read">End-User License Argeement</a></li>
+        </ul>
+        </div>
+        <div class="card-footer">&copy; Nawin 2021 All right reserved.</div>
+    </div>
+</div>
+<?php require_once('./../static/Content/foot.php') ?>
